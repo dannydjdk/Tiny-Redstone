8e6699bfeec7cff63b82c2e47155d0e50d2bd5dd
danny <dannydjdk@users.noreply.github.com>
2021-04-16 00:31:42 -0500

    -Allow setting Tiny Super Repeater delay to 0 ticks to act as 0 tick diode. -Updated Repeater rendering code and other code cleanup. -Localized text for Tiny Redstone Bridge.

--- src/main/java/com/dannyandson/tinyredstone/blocks/panelcells/SuperRepeater.java
+++ src/main/java/com/dannyandson/tinyredstone/blocks/panelcells/SuperRepeater.java
@@ -1,27 +1,58 @@
 package com.dannyandson.tinyredstone.blocks.panelcells;
 
 import com.dannyandson.tinyredstone.TinyRedstone;
-import com.dannyandson.tinyredstone.blocks.PanelCellPos;
-import com.dannyandson.tinyredstone.blocks.PanelCellSegment;
-import com.dannyandson.tinyredstone.blocks.PanelTile;
+import com.dannyandson.tinyredstone.blocks.*;
 import com.dannyandson.tinyredstone.gui.RepeaterCellGUI;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nullable;
 
 public class SuperRepeater extends Repeater {
 
     public static ResourceLocation TEXTURE_SUPER_REPEATER_ON = new ResourceLocation(TinyRedstone.MODID,"block/panel_super_repeater_on");
     public static ResourceLocation TEXTURE_SUPER_REPEATER_OFF = new ResourceLocation(TinyRedstone.MODID,"block/panel_super_repeater_off");
 
-    /**
-     * Called when the cell is activated. i.e. player right clicked on the cell of the panel tile.
-     *
-     * @param cellPos The position of the clicked IPanelCell within the panel (this IPanelCell)
-     * @param segmentClicked Which of nine segment within the cell were clicked.
-     * @return true if a change was made to the cell output
-     */
+    @Override
+    public boolean neighborChanged(@Nullable PanelCellNeighbor frontNeighbor,@Nullable PanelCellNeighbor rightNeighbor,@Nullable  PanelCellNeighbor backNeighbor,@Nullable  PanelCellNeighbor leftNeighbor)
+    {
+        boolean changed=false;
+        if (backNeighbor!=null && backNeighbor.getWeakRsOutput() >0 && !input)
+        {
+            input=true;
+        }
+        else if ((backNeighbor==null || backNeighbor.getWeakRsOutput() ==0 ) && input)
+        {
+            input=false;
+        }
+
+        this.locked= (leftNeighbor != null && leftNeighbor.getStrongRsOutput() > 0 &&
+                (leftNeighbor.getNeighborIPanelCell() instanceof Repeater || leftNeighbor.getNeighborIPanelCell() instanceof Comparator ||
+                        (leftNeighbor.getNeighborBlockState() != null && (leftNeighbor.getNeighborBlockState().getBlock() == Blocks.REPEATER || leftNeighbor.getNeighborBlockState().getBlock() == Blocks.COMPARATOR))
+                ))
+                ||
+                (rightNeighbor != null && rightNeighbor.getStrongRsOutput() > 0 &&
+                        (rightNeighbor.getNeighborIPanelCell() instanceof Repeater || rightNeighbor.getNeighborIPanelCell() instanceof Comparator ||
+                                (rightNeighbor.getNeighborBlockState() != null && (rightNeighbor.getNeighborBlockState().getBlock() == Blocks.REPEATER || rightNeighbor.getNeighborBlockState().getBlock() == Blocks.COMPARATOR))
+                        ));
+
+        if (this.ticks==0 && ! this.locked && this.input!=this.output)
+        {
+            this.output=this.input;
+            changed=true;
+        }
+
+        return changed;
+    }
+
+        /**
+         * Called when the cell is activated. i.e. player right clicked on the cell of the panel tile.
+         *
+         * @param cellPos The position of the clicked IPanelCell within the panel (this IPanelCell)
+         * @param segmentClicked Which of nine segment within the cell were clicked.
+         * @return true if a change was made to the cell output
+         */
     @Override
     public boolean onBlockActivated(PanelCellPos cellPos, PanelCellSegment segmentClicked) {
         PanelTile panelTile = cellPos.getPanelTile();
@@ -34,8 +65,8 @@ public class SuperRepeater extends Repeater {
     protected TextureAtlasSprite getRepeaterTexture()
     {
         if (this.output)
-            return Minecraft.getInstance().getAtlasSpriteGetter(AtlasTexture.LOCATION_BLOCKS_TEXTURE).apply(TEXTURE_SUPER_REPEATER_ON);
-        return Minecraft.getInstance().getAtlasSpriteGetter(AtlasTexture.LOCATION_BLOCKS_TEXTURE).apply(TEXTURE_SUPER_REPEATER_OFF);
+            return RenderHelper.getSprite(TEXTURE_SUPER_REPEATER_ON);
+        return RenderHelper.getSprite(TEXTURE_SUPER_REPEATER_OFF);
 
     }
 
