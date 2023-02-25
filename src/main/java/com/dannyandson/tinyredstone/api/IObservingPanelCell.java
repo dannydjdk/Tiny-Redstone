2930246172a8789003d9602746256a181c20fae0
danny <dannydjdk@users.noreply.github.com>
2023-02-24 21:35:44 -0600

    Fix observer responding differently to ticking signals coming from different directions #120

--- src/main/java/com/dannyandson/tinyredstone/api/IObservingPanelCell.java
+++ src/main/java/com/dannyandson/tinyredstone/api/IObservingPanelCell.java
@@ -1,8 +1,10 @@
 package com.dannyandson.tinyredstone.api;
 
+import com.dannyandson.tinyredstone.blocks.PanelCellPos;
+
 /**
  * Interface for Tiny Observer-like components to receive notice when observing cell changes.
  */
 public interface IObservingPanelCell {
-    boolean frontNeighborUpdated();
+    boolean frontNeighborUpdated(PanelCellPos cellPos);
 }
