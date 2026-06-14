f0b5653d56c005a1dffd7191fb6295dbcb1feef4
Danny <danny@dannyandson.com>
2026-06-14 10:54:16 -0500

    Updated and enabled reload listener for tiny blocks to disable or override textures for certain blocks to use as tiny blocks. Fixed error in more_tiny_blocks.json and renamed to tiny_block_exclusions.json to better describe its contents.

--- src/main/java/com/dannyandson/tinyredstone/setup/ModSetup.java
+++ src/main/java/com/dannyandson/tinyredstone/setup/ModSetup.java
@@ -1,10 +1,11 @@
 package com.dannyandson.tinyredstone.setup;
 
 import com.dannyandson.tinyredstone.TinyRedstone;
+import net.minecraft.resources.Identifier;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-// TODO: AddReloadListenerEvent was removed/renamed in 26.1.
-// Find the replacement event to re-register TINY_BLOCK_OVERRIDES.
+import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 
 @EventBusSubscriber(modid = TinyRedstone.MODID)
 public class ModSetup {
@@ -13,10 +14,12 @@ public class ModSetup {
         ModRegistration.registerPanelCells();
     }
 
-    // TODO: Re-enable reload listener once the correct 26.1 event is identified
-    // @SubscribeEvent
-    // public static void onAddReloadListeners(??? event) {
-    //     event.addListener(Registration.TINY_BLOCK_OVERRIDES);
-    // }
+    // Registers the data-pack listener that loads tiny_block_overrides JSON.
+    @SubscribeEvent
+    public static void onAddReloadListeners(final AddServerReloadListenersEvent event) {
+        event.addListener(
+                Identifier.fromNamespaceAndPath(TinyRedstone.MODID, "tiny_block_overrides"),
+                ModRegistration.TINY_BLOCK_OVERRIDES);
+    }
 
 }
